3b630a542d5f4adae9b822f55650483a6f14eed2
Rahim Kaan Kacar <rahimkaanis@hotmail.com>
2024-12-20 20:04:50 +0300

    View dosyaları eklendi ve ProductViewModel eklendi.

--- FormsApp/obj/Debug/net8.0/FormsApp.AssemblyInfo.cs
+++ FormsApp/obj/Debug/net8.0/FormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48c75ddea6a9237b31bc5c0255f27bca54d6406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14b0a9e919fb527bfc8e10bd8978574998aea6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
